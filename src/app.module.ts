e2b61b6a5038eb83cb95afb72e1e55038802037c
Дмитрий Варганов <varganovda@gmail.com>
2023-06-26 23:10:57 +0300

    feat(code): added a server static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin@cluster0.38clpiv.mongodb.net/music-platform?retryWrites=true&w=majority',
     ),
