3e6d3be16942695284778b9c3b25673cd80ac2a1
Дмитрий Варганов <varganovda@gmail.com>
2023-06-28 16:03:28 +0300

    feat(code, ui): added a tracks page content

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import MainLayout from '../../layouts/MainLayout';
+import { Box, Button, Card, Grid } from '@mui/material';
+import { useRouter } from 'next/router';
+import { ITrack } from '../../types/track';
 
 const Index = () => {
+  const router = useRouter();
+  const tracks: ITrack[] = [
+    {_id: '1', name: 'Трек 1', artist: 'Исполнитель 1', listened: 0, picture: '', audio: '', text: '', comments: []},
+    {_id: '2', name: 'Трек 2', artist: 'Исполнитель 2', listened: 0, picture: '', audio: '', text: '', comments: []},
+    {_id: '3', name: 'Трек 3', artist: 'Исполнитель 3', listened: 0, picture: '', audio: '', text: '', comments: []},
+  ];
+
   return (
-    <div>
-      Список треков
-    </div>
+    <MainLayout>
+      <Grid container justifyContent='center'>
+        <Card style={{width: '900px'}}>
+          <Box p={3}>
+            <Grid container justifyContent='space-between'>
+              <h1>Список треков</h1>
+              <Button onClick={() => router.push('/tracks/create')}>
+                Загрузить
+              </Button>
+            </Grid>
+          </Box>
+        </Card>
+      </Grid>
+    </MainLayout>
   );
 };
 
