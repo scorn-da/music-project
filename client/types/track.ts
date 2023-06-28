7407ec0dc149e3e19c29f3a2bc8b59aa42672355
Дмитрий Варганов <varganovda@gmail.com>
2023-06-28 16:03:00 +0300

    feat(code): added interfaces for tracks

---
+++ client/types/track.ts
@@ -0,0 +1,16 @@
+export interface Icomment {
+  _id: string;
+  username: string;
+  text: string;
+}
+
+export interface ITrack {
+  _id: string;
+  name: string;
+  artist: string;
+  text: string;
+  listened: number;
+  picture: string;
+  audio: string;
+  comments: IComment[];
+}
