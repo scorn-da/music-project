3a005855cb8e3437a59d7a3a49a3b6388f9e922b
Дмитрий Варганов <varganovda@gmail.com>
2023-06-28 15:13:48 +0300

    refactor(code, ui): updated the nav with pages routes

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -7,7 +7,6 @@ import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
@@ -18,6 +17,13 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { useRouter } from 'next/router';
+
+const menuItems = [
+  { text: 'Главная', href: '/' },
+  { text: 'Список треков', href: '/tracks' },
+  { text: 'Список альбомов', href: '/albums' },
+];
 
 const drawerWidth = 240;
 
@@ -54,6 +60,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 export default function Navbar() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const router = useRouter();
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -101,9 +108,9 @@ export default function Navbar() {
           </IconButton>
         </DrawerHeader>
         <List>
-          {['Главная', 'Список треков', 'Список альбомов'].map((text, index) => (
+          {menuItems.map(({ text, href }, index) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
+              <ListItemButton onClick={() => router.push(href)}>
                 <ListItemIcon>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
