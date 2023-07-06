55ae3210c040d2b9b1934f4cc0d280a051c9e116
Дмитрий Варганов <varganovda@gmail.com>
2023-07-07 00:13:18 +0300

    feat(code): inited a FileUpload component

---
+++ client/components/FileUpload/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface FileUploadProps {
+  file: any;
+  setFile: Function;
+
+}
+
+const FileUpload: React.FC<FileUploadProps> = ({ file, setFile }) => {
+  return (
+    <div>
+      <input type='file' />
+    </div>
+  );
+};
+
+export default FileUpload;
