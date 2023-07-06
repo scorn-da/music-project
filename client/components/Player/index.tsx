54cc3cdceb415884e33cf16b23e5a3296ada9b14
Дмитрий Варганов <varganovda@gmail.com>
2023-07-07 00:46:37 +0300

    feat(code): added a Player component

---
+++ client/components/Player/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IconButton } from '@mui/material';
+import { Pause, PlayArrow } from '@mui/icons-material';
+import styles from 'Player.module.scss';
+
+interface PlayerProps {
+
+}
+
+const Player = () => {
+  const active = false;
+
+  return (
+    <div className={styles.player}>
+      <IconButton onClick={(e) => e.stopPropagation()}>
+        {!active
+          ? <PlayArrow/>
+          : <Pause />
+        }
+      </IconButton>
+    </div>
+  );
+};
+
+export default Player;
