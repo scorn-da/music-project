a444b516dee538c3d4d6a8e3ede7cd4995c0c21e
Дмитрий Варганов <varganovda@gmail.com>
2023-07-16 19:53:43 +0300

    feat(code, store): added a store and it's types

---
+++ client/store/index.ts
@@ -0,0 +1,7 @@
+import { Context, createWrapper, MakeStore } from 'next-redux-wrapper';
+import { createStore, Store } from 'redux';
+import { reducer, RootState } from './reducers';
+
+const makeStore = (context: Context) => createStore(reducer);
+
+export const wrapper = createWrapper<Store<RootState>>(makeStore, {debug: true});
