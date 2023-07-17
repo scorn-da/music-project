5917b47937b68b02e9b71484c96b7f049dd0d90f
Дмитрий Варганов <varganovda@gmail.com>
2023-07-17 13:21:34 +0300

    feat(ui): added the track playing

--- client/components/Player/index.tsx
+++ client/components/Player/index.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, IconButton } from '@mui/material';
 import { Pause, PlayArrow, VolumeUp } from '@mui/icons-material';
 import styles from './Player.module.scss';
 import { ITrack } from '../../types/track';
 import TrackProgress from '../TrackProgress';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { useActions } from '../../hooks/useActions';
 
 interface PlayerProps {
 
 }
 
-const Player = () => {
-  const track: ITrack = {_id: '1', name: 'Трек 1', artist: 'Исполнитель 1', listened: 0, picture: '', audio: '', text: '', comments: []};
-  const active = false;
+let audio;
 
+const Player = () => {
+  const track: ITrack = {_id: '1', name: 'Трек 1', artist: 'Исполнитель 1', listened: 0, picture: 'http://localhost:5000/image/06c9c5a3-2ebc-4dbc-b559-47f4eb66b254.webp', audio: 'http://localhost:5000/audio/f7ef3b6a-a0cb-4726-b5f0-492d4e4d28c1.mp3', text: 'Какой-то текст', comments: []};
+  const { pause, active, duration, volume, currentTime } = useTypedSelector(state => state.player);
+  const { pauseTrack, playTrack } = useActions();
+
+  useEffect(() => {
+    if (!audio) {
+      audio = new Audio;
+      audio.src = track.audio;
+    }
+  }, [])
+
+  const play = () => {
+    if (pause) {
+      playTrack();
+      audio.play();
+    } else {
+      pauseTrack();
+      audio.pause();
+    }
+  }
 
   return (
     <div className={styles.player}>
-      <IconButton onClick={(e) => e.stopPropagation()}>
-        {!active
+      <IconButton onClick={play}>
+        {pause
           ? <PlayArrow/>
           : <Pause />
         }
       </IconButton>
+      <img src={track.picture} alt={`${track.artist} — ${track.name}`} style={{width: 40, height: 40}} />
       <Grid container direction='column' style={{width: 200, margin: '0 20px'}}>
         <strong>{track.name}</strong>
         <i>{track.artist}</i>
