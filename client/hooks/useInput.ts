c8a1dfef4bd37d370a282d1aac6fc4e97896e36b
Дмитрий Варганов <varganovda@gmail.com>
2023-07-18 15:53:12 +0300

    feat(code): typed the useInput hook

--- client/hooks/useInput.ts
+++ client/hooks/useInput.ts
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 
 export const useInput = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
+  const [value, setValue] = useState<string>(initialValue);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
